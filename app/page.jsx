26203dc8ae810b1f67beefc0b0a45e08f33e5027
iamhitesh94 <iamhitesh3@gmail.com>
2024-10-05 23:49:14 +0530

    Fix qoute issue and add sharp module

--- app/page.jsx
+++ app/page.jsx
@@ -13,10 +13,10 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Web Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm<br />
+              Hello I&apos;m<br />
               <span className="text-accent">Hitesh Solanki</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p className="max-w-[500px] mb-9 text-white/80">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
             <div className="hier-social-btns flex flex-col xl:flex-row items-center gap-2">
               <Link href="/contact" className="mb-5 xl:mb-0">
